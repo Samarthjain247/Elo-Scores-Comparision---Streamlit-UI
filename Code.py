082786f1f8ab14ede577e61b4aa652d6b2501e52
samarthjain247 <samarthjain247@gmail.com>
2024-12-01 23:40:36 +0530

    check2

--- Code.py
+++ Code.py
@@ -5,6 +5,7 @@ import pandas as pd
 from random import shuffle, choice
 import csv
 import os
+import csv
 
 # Define checkpoints how many you want to compare
 checkpoints = ['ckpt1', 'ckpt2', 'ckpt3', 'ckpt4', 'ckpt5', 'ckpt6', 'ckpt7', 'ckpt8']
